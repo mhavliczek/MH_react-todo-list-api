30926e31e6067fc7dbecf7ca38bf892a478972fb
mhavliczek <mhavliczek@gmail.com>
2024-10-09 21:07:00 -0300

    Interaccion con Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,88 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
+const apiUrls = {
+  username: 'https://playground.4geeks.com/todo/users/havli',
+  todos: 'https://playground.4geeks.com/todo/todos/',
+  create: 'https://playground.4geeks.com/todo/todos/havli',
+};
+
 const App = () => {
-  const api_url_username = 'https://playground.4geeks.com/todo/users/ACA_SU_USUARIO'; //Modificar el nombre de usuario con el valor del usuario que crearon
-  const api_url_todos = 'https://playground.4geeks.com/todo/todos/';
   const [username, setUsername] = useState('');
   const [usertodos, setUserTodos] = useState({});
-  const [flagerror, setFlagError] = useState(false);
+  const [error, setError] = useState(null);
+  const [newtask, setNewtask] = useState('');
 
   useEffect(() => {
     getListTodos();
-  	//return () => '';
+    return () => {
+      // Función de limpieza
+    };
   }, []);
 
+  const postToDo = async () => {
+    try {
+      const response = await fetch(apiUrls.create, {
+        method: 'POST',
+        body: JSON.stringify({ label: newtask, is_done: false }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const data = await response.json();
+      getListTodos();
+      return data;
+    } catch (error) {
+      console.error('Error:', error);
+      setError(error);
+    }
+  };
+
   const getListTodos = async () => {
-    const response = await fetch(api_url_username);
-    if (response.ok) {
+    try {
+      const response = await fetch(apiUrls.username);
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
       const data = await response.json();
       setUsername(data.name);
       setUserTodos(data.todos);
-      console.log(data);
-    } else {
-      console.log('Error: ', response.status, response.statusText);
-      setFlagError(true);
-      return {error: {status: response.status, statusText: response.statusText}};
-    };
+    } catch (error) {
+      console.error('Error:', error);
+      setError(error);
+    }
   };
 
   const updateTodo = async (id, label, is_done) => {
-    let data = {
-                'label': label,
-                'is_done': is_done
-    };
-
-    const response = await fetch(api_url_todos + id, {
-      method: 'PUT',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
+    try {
+      const response = await fetch(`${apiUrls.todos}${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ label, is_done }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (!response.ok) {
+        throw new Error(response.statusText);
       }
-    });
-
-    //Acá el código que actualiza el state usertodos
+    } catch (error) {
+      console.error('Error:', error);
+      setError(error);
+    }
   };
 
   const deleteTodo = async (id) => {
-    const response = await fetch(api_url_todos + id, {
-      method: 'DELETE'
-    });
-
-    //Acá el código que actualiza el state usertodos
+    try {
+      const response = await fetch(`${apiUrls.todos}${id}`, {
+        method: 'DELETE',
+      });
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      getListTodos();
+    } catch (error) {
+      console.error('Error:', error);
+      setError(error);
+    }
   };
 
   return (
@@ -57,49 +90,48 @@ const App = () => {
       <header className='header'>
         <h1>Todo List</h1>
       </header>
-      {flagerror ? 
-      <section className='error-notice'>
-        <div class="oaerror danger">
-          <strong>Error:</strong> Ha ocurrido un error en la carga del listado de tareas
-        </div>
-        
-      </section>
-      :
-      <>
-      <section className='todo-input-section'>
-        <div className='todo-input-wrapper'>
-          <input type='text' id='todo-input' placeholder='Escribe la tarea' />
-          <button id='add-button'>Agregar</button>
-        </div>
-      </section>
+      {error ? (
+        <section className="error-notice">
+          <div className="oaerror danger">
+            <strong>Error:</strong> {error.message}
+          </div>
+        </section>
+      ) : (
+        <>
+          <section className='todo-input-section'>
+            <div className='todo-input-wrapper'>
+              <input type='text' id='todo-input' placeholder='Escribe la tarea' onChange={(e) => { setNewtask(e.target.value) }} />
+              <button id='add-button' onClick={() => { postToDo() }}>Agregar</button>
+            </div>
+          </section>
 
-      <section className='todo-list-section'>
-        {usertodos && usertodos.length ?
-          <ul id='todo-list'>
-            {usertodos.map((todo) => 
-              <li className='todo-item' key={todo.id}>
-                {
-                  !todo.is_done ? 
-                  <>
-                  <span className='task-text'>{todo.label}</span>
-                  <button className='complete-button' onClick={() => { updateTodo(todo.id, todo.label, true) }}>Marcar como hecha</button>
-                  </>
-                  :
-                  <>
-                  <span className='task-text is-done'>{todo.label}</span>
-                  <button className='complete-button' onClick={() => { updateTodo(todo.id, todo.label, false) }}>Marcar como no hecha</button>
-                  </>
-                }
-                <button className='delete-button' onClick={ () => { deleteTodo(todo.id) } }>Eliminar</button>
-              </li>
-            )}
-          </ul>
-        :
-          <div className="dots"></div>
-        }
-      </section>
-      </>
-      }
+          <section className='todo-list-section'>
+            {usertodos && usertodos.length ?
+              <ul id='todo-list'>
+                {usertodos.map((todo) =>
+                  <li className='todo-item' key={todo.id}>
+                    {
+                      !todo.is_done ?
+                        <>
+                          <span className='task-text'>{todo.label}</span>
+                          <button className='complete-button' onClick={() => { updateTodo(todo.id, todo.label, true) }}>Marcar como hecha</button>
+                        </>
+                        :
+                        <>
+                          <span className='task-text is-done'>{todo.label}</span>
+                          <button className='complete-button' onClick={() => { updateTodo(todo.id, todo.label, false) }}>Marcar como no hecha</button>
+                        </>
+                    }
+                    <button className='delete-button' onClick={() => { deleteTodo(todo.id) }}>Eliminar</button>
+                  </li>
+                )}
+              </ul>
+              :
+              <div className="dots"></div>
+            }
+          </section>
+        </>
+      )}
     </main>
   );
 }
